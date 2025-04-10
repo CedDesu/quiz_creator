ae679a52094b112bab8bb78362c68b46c90e8c41
CedDesu <cedriqueorbista@gmail.com>
2025-04-10 17:35:37 +0800

    Wrote basic code structure that includes the user input for the question, the 4 choices, option to write another question, and a placeholder for the code that saves the input questions.

---
+++ quiz_creator_main.py
@@ -0,0 +1,14 @@
+"""
+print("Welcome to Quiz_Creator v.1, please press [Enter] to start.")
+
+input_question = input("")
+
+choices = {}
+for choices in []:
+    answer = input()
+
+next_question = input("")
+
+save to file code
+"""
+

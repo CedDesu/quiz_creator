fc30c1d85df9ec67ee04eb32d7a557328ea4c4fb
CedDesu <cedriqueorbista@gmail.com>
2025-04-10 18:39:31 +0800

    Added a while loop for the input functions that asks the user for the questions and choices. Used a for loop to ask from 'a' until 'd' for the choices and then added 'choices[] =' variable to add to the 'choices = {}' dictionary.

--- quiz_creator_main.py
+++ quiz_creator_main.py
@@ -12,7 +12,18 @@ save to file code
 
 def quiz_creator():
 
-    input("Welcome to Quiz_Creator v.1, please press any button to start.")
+    input("Welcome to Quiz_Creator v.1, please press any button to start.\n")
+
+    #Asks the user for a question and 4 choices
+    while True:
+
+        input_question = input("Enter a question: \n")
+
+        choices = {}
+        for choice in ['a', 'b', 'c', 'd']:
+            answer = input(f"Enter choice {choice}")
+            choices[choice] = answer
+
 
 
 

b48c3b5de18121627f84cc2a5cbdacc13e07f064
CedDesu <cedriqueorbista@gmail.com>
2025-05-08 20:59:25 +0800

    Added a block of code that's used to open the txt file made by the main code

--- quiz_taker.py
+++ quiz_taker.py
@@ -4,3 +4,7 @@ Quiz taker - 2nd part of quiz creator which uses tkinter library to open a new w
 """
 
 import tkinter as tk
+
+def load_questions_from_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = [line.strip() for line in file if line.strip()]

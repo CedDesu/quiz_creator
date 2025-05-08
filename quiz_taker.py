68ec70143a2398c1a6c6789673a4944a4fc2b100
CedDesu <cedriqueorbista@gmail.com>
2025-05-08 22:37:18 +0800

    Finished list block code

--- quiz_taker.py
+++ quiz_taker.py
@@ -10,5 +10,12 @@ def load_questions_from_file(filename):
         lines = [line.strip() for line in file if line.strip()]
 
     questions_data = []
-    for index in range(0, len(lines), 6):
-        if lines[index].startswith("Q") and lines[index
+      for index in range(0, len(lines), 6):
+        if lines[index].startswith("Q") and lines[index +
+    5].startswith("Correct answer:"):
+            question_text = lines[index]
+            choices = lines[index + 1 : index + 5]
+            correct_answer_line = lines[index + 5]
+            questions_data.append((question_text, choices,
+    correct_answer_line))
+        return questions_data

0dadf3fe8eb09c41bb091dd769df95a4148604d8
CedDesu <cedriqueorbista@gmail.com>
2025-05-08 22:22:54 +0800

    Attempted to write a code that, if formatted properly, will put the questions in a list. Will finish soon

--- quiz_taker.py
+++ quiz_taker.py
@@ -8,3 +8,7 @@ import tkinter as tk
 def load_questions_from_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         lines = [line.strip() for line in file if line.strip()]
+
+    questions_data = []
+    for index in range(0, len(lines), 6):
+        if lines[index].startswith("Q") and lines[index
